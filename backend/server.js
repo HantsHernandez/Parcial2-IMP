fa24c3c320967f17457762b0a09e08026cc1529e
HantsHernandez <hants.sabo1323@gmail.com>
2025-10-24 21:05:48 -0600

    feat: obtenccion de datos y aviso de conexion

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,49 @@
 import express from 'express';
-const app = express();
+import pkg from 'pg';
+const { Pool } = pkg;
 
+const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.json({
-    nombre: 'Hants hernandez',
-    expediente: '25614'
-  });
+const pool = new Pool({
+  host: process.env.DB_HOST || 'localhost',
+  user: process.env.DB_USER || 'admin',
+  password: process.env.DB_PASSWORD || '12345',
+  database: process.env.DB_NAME || 'parcial_db',
+  port: Number(process.env.DB_PORT) || 5432,
 });
+
+app.use(express.json());
+
+const checkDBConnection = async () => {
+  try {
+    await pool.query('SELECT 1'); 
+    console.log('conexin a la DB exitosa');
+  } catch (err) {
+    console.error('errir', err.message);
+    setTimeout(checkDBConnection, 3000);
+  }
+};
+
+checkDBConnection();
+
+app.get('/', async (_req, res) => {
+  try {
+    const result = await pool.query('SELECT * FROM estudiantes');
+    res.json({
+      dbStatus: 'Conexionx exitosa',
+      estudiantes: result.rows,
+    });
+  } catch (err) {
+    res.status(500).json({ error: 'Error conectando ', details: err.message });
+  }
+});
+
 
 app.get('/health', (_req, res) => {
   res.json({ status: 'OK' });
 });
 
 app.listen(PORT, () => {
-  console.log(`si jala puerto ${PORT}`);
+  console.log(`Servidor escuchando en puerto ${PORT}`);
 });
